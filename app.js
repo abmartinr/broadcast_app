baa2886e06ac1d1f8e3545eb50b7e0a36e555d45
Abel Martín Romero <aromero@zendesk.com>
2014-09-30 01:57:07 +0200

    Working
    Removed translation error
    Added group filter
    notify.json not found on broadcast sent

--- app.js
+++ app.js
@@ -2,11 +2,49 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'checkGroup',
+      'broadcastListener' : 'broadcastReceived',
+      'click .notify_button' :  function() {
+    		this.ajax('sendBroadcast', "This is the message").done(function(){return true;});
+      	}
+      },
+    //Send the broadcast message
+    requests: {
+      sendBroadcast : function(data){
+      	return{
+      		url: '/api/v2/apps/notify',
+      		type: 'POST',
+      		contentType: 'aplication/json',
+      		data: JSON.stringify({
+      			"app_id" : 0,
+      			"event" : "broadcastListener",
+      			"body" : data
+      		})
+      	}
+      }
+	},
+    //We receive the request and set the alerts
+    broadcastReceived: function(body) {
+    	alert(body);
     },
-
-    doSomething: function() {
+    //We check the group to see which interface to load.
+    checkGroup: function() {
+    	var admin = false;
+    	var groups = this.currentUser().groups();
+    	for(var i = 0 ; i< groups.length; i++){
+    		if (groups[i].id() == this.setting('sender') ){
+    			admin = true;
+    		}
+    	}
+    	if(admin){
+    		this.switchTo('admin');
+    	}else{
+    		this.switchTo('agent');
+    	}
     }
+
+
+
   };
 
 }());
